ad863915bf1a6d6250624e7ded83d776cd23d723
AlKhrisW <aldokhrisna@gmail.com>
2025-09-30 21:04:58 +0700

    Tambah dialog widget & modifikasi main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/basic_widgets/scaffold_widget.dart';
+import 'package:hello_world/basic_widgets/dialog_widget.dart';
 
 void main() {
-  runApp(const ScaffoldWidget());
+  runApp(const DialogWidget());
 }
