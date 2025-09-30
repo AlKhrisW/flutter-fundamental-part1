5ec1cba7d1cca2100bc34a965b16d46a70af4e81
AlKhrisW <aldokhrisna@gmail.com>
2025-09-30 20:55:52 +0700

    Tambah scaffold widget & modifikasi main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/basic_widgets/fab_widget.dart';
+import 'package:hello_world/basic_widgets/scaffold_widget.dart';
 
 void main() {
-  runApp(const FabWidget());
+  runApp(const ScaffoldWidget());
 }
